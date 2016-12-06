937fa1c9fbce95d801141005eab08c4af7e20d03
alichen <ali322@gmail.com>
2016-12-06 14:19:30 +0800

    project init

---
+++ README.md
@@ -0,0 +1,32 @@
+Nva-API [![npm version](https://badge.fury.io/js/nva-api.svg)](https://badge.fury.io/js/nva-api)
+===
+[![NPM](https://nodei.co/npm/nva-api.png)](https://nodei.co/npm/nva-api/)
+
+yet another fetch adapter
+
+Install
+===
+
+```javascript
+npm install nva-api --save
+```
+
+Useage
+===
+
+```javascript
+import api from 'nva-api'
+api.get("http://example.com",{q:'search'}).then(ret=>{
+    console.log('ret',ret)
+})
+```
+
+## Todo
+
+- fix some unknow bugs
+- add more api documents
+
+
+## License
+
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
---
+++ index.js
@@ -0,0 +1,32 @@
+import qs from "qs"
+
+const fetch = typeof window === "object"?window.fetch:global.fetch
+
+if(!fetch){
+    throw new Error('A global `fetch` method is required as either `window.fetch` for browsers or `global.fetch` for node runtime environments')
+}
+
+const api = {
+    get(url,data={},headers={}){
+        data = qs.stringify(data)
+        url = `${url}?${data}`
+        return fetch(url,headers).then(ret=>ret.json())
+    },
+    post(url,data={},headers={}){
+        return fetch(url,{
+            method:"POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+                ...headers
+            },
+            body:JSON.stringify(data)
+        }).then((ret)=>{
+            if(ret.ok){
+                return ret.json()
+            }else{
+                throw new Error(`post failed,err stack:${ret.error?ret.error():""}`)
+            }
+        })
+    }
+}
